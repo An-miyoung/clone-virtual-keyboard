ba5bbbd7fcf23a19edf642eeca4cfe5b244f467e
miyoung <noonchicat@gmail.com>
2022-09-15 14:22:44 +0900

    refactoring

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,26 +1,32 @@
 export class Keyboard {
   #swichEl;
   #fontSelectEl;
+  #containerEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
 
   #assignElement() {
-    this.#swichEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (e) => {
-      // html 을 handling 하기 위해 documentElement 를 사용
-      document.documentElement.setAttribute(
-        "theme",
-        e.target.checked ? "dark-mode" : ""
-      );
-    });
-    this.#fontSelectEl.addEventListener("change", (e) => {
-      document.body.style.fontFamily = e.target.value;
-    });
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onchangeFont);
   }
+
+  #onChangeTheme = (e) => {
+    // html 을 handling 하기 위해 documentElement 를 사용
+    document.documentElement.setAttribute(
+      "theme",
+      e.target.checked ? "dark-mode" : ""
+    );
+  };
+
+  #onchangeFont = (e) => {
+    document.body.style.fontFamily = e.target.value;
+  };
 }
